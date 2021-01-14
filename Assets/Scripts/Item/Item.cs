bd92d40de8a2604af2bba732e823629a40706749
meysam ameri <maisamflash@gmail.com>
2021-01-14 21:18:50 +0330

    Create itemSO.cs.this is a scriptableObject script for creating items from menu Create item.cs for creating items visually the item get its visual data of itemSO

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField]private Animator _animator;
+
+    private ItemType _type;
+    private int _score;
+    
+    public bool IsSwitch { get; set; }
+
+    public void Init(ItemSO data)
+    {
+        _renderer.sprite = data.Sprite;
+        _type = data.ItemType;
+        _score = data.Score;
+    }
+    
+}
---
+++ Assets/Scripts/Item/ItemSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Apple , Banana , Orange
+}
+[CreateAssetMenu(fileName = "Item" , menuName = "Create new item")]
+public class ItemSO : ScriptableObject
+{
+    public ItemType ItemType;
+    public Sprite Sprite;
+    public int Score;
+}
