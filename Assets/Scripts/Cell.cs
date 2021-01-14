7461dbfc24118f6a40fe1528c9430d5e816280b9
meysam ameri <maisamflash@gmail.com>
2021-01-15 00:29:38 +0330

    Create cell class Create cell prefab

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+
+    private bool _isFree;
+    public bool IsFree
+    {
+        get { return _isFree; }
+        set { _isFree = value; }
+    }
+
+    private int _row;
+    public int Row => _row;
+
+    private int _column;
+    public int Column=> _column;
+
+    private Item _item;
+    public Item Item
+    {
+        get { return _item; }
+        set { _item = value; }
+    }
+
+    [SerializeField] private SpriteRenderer _renderer;
+    public SpriteRenderer Renderer => _renderer;
+
+    [SerializeField] private TextMeshPro _textMesh;
+
+    
+    public void Init(int row , int coulmn)
+    {
+        _row = row;
+        _column = coulmn;
+        _textMesh.text = $"{_row} : {_column}";
+    }
+
+
+}
