071ed81a64d1222cd8aa85f9d34df41ffdde487a
meysam ameri <maisamflash@gmail.com>
2021-01-15 18:57:04 +0330

    Edit levelManager for creating items into ever cell the items create randomly

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private GameObject _framePrefab;
     [SerializeField] private GameObject _cellPrefab;
+    [SerializeField] private GameObject _itemPrefab;
 
     [SerializeField] private float _xCrop;
     [SerializeField] private float _yCrop;
@@ -65,8 +67,24 @@ public class LevelManager : MonoBehaviour
                 cell.transform.position = new Vector3(leftDownPoint.x + width * column , leftDownPoint.y + height * row);
                 cell.Init(row , column);
                 _cells[row, column] = cell;
+                CreateItem(row , column);
             }    
         }
 
     }
+    
+    // Create items ***************************
+
+    [SerializeField] private ItemSO[] _itemsData;
+
+    private void CreateItem(int row , int column)
+    {
+        var cell = _cells[row, column];
+        var item = Instantiate(_itemPrefab, cell.transform.position, quaternion.identity).GetComponent<Item>();
+        var index = Random.Range(0, 3);
+        var data = _itemsData[index];
+        item.Init(data);
+        cell.Item = item;
+    }
+    
 }
